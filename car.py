b4af22db917397d8f1227e2aa1ab2b39145f5ead
Sidharth S <sidharth.s16@yahoo.com>
2021-06-08 20:34:47 +0100

    Fixed nonetype error

--- car.py
+++ car.py
@@ -17,23 +17,23 @@ radio.config(channel=19)
 speed_left = 0      # Speed of the right motor from 0 (off) to 100 (max)
 speed_right = 0     # Speed of the right motor from 0 (off) to 100 (max)
 
-delay_ms = 10
+delay_ms = 50
 
 while True:
     acceleration_status = 0
-    message_string_received = radio.receive()
-    if message_string_received != None:
+    message_string_received = str(radio.receive())
+    if message_string_received != "None":
         acceleration_status = float(message_string_received[3:])
 
-    if acceleration_status > 0.5: acceleration_status = 0.5
+        if acceleration_status > 0.5: acceleration_status = 0.5
 
-    speed_left = (acceleration_status*200) * message_string_received[2]
-    speed_right = (acceleration_status*200) * message_string_received[1]
+        speed_left = (acceleration_status*200) * message_string_received[2]
+        speed_right = (acceleration_status*200) * message_string_received[1]
 
-    analogue_out_left = (speed_left * 1023)/100
-    analogue_out_right = (speed_right * 1023)/100
-    
-    pin1.write_analog(analogue_out_left)
-    pin2.write_analog(analogue_out_right)
+        analogue_out_left = (speed_left * 1023)/100
+        analogue_out_right = (speed_right * 1023)/100
+        
+        pin1.write_analog(analogue_out_left)
+        pin2.write_analog(analogue_out_right)
 
     sleep(delay_ms)
